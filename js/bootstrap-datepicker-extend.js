73adf11707ec1cb80123591c66d9fd50924d6ec9
王子昱 <wang_ziyu@vip.sina.com>
2017-03-16 16:41:09 +0800

    王子昱的第三次修改提交
    王子昱的第三次修改提交

---
+++ js/bootstrap-datepicker-extend.js
@@ -0,0 +1,25 @@
+jQuery.datepickerPlugin = function(parameters) {
+	
+	// 变量声明
+	
+	var formatDate = function(y, m, d) {
+		y = y;
+		m = m + 1 < 10 ? ('0' + (m + 1)) : m + 1;
+		d = d < 10 ? ('0' + d) : d;
+		return y + '-' + m + '-' + d;
+	};
+	
+	if (parameters.customSelector == true) {
+		itemNumber++;
+	}
+	if (parameters.monthSelector == true) {
+		itemNumber++;
+	}
+	if (parameters.weekSelector == true) {
+		itemNumber++;
+	}
+	if (itemNumber == 0) {
+		alert("无法生成日期选择插件，错误原因：无选择项！");
+		return;
+	}
+}
